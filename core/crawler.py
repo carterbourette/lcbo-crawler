6ade05ce8b7a265d8109d450507281cf14afdf07
carterbourette <cbourette@gmail.com>
2018-12-17 21:13:53 -0500

    Mitigate RemoteDisconnected error

--- core/crawler.py
+++ core/crawler.py
@@ -12,7 +12,7 @@ class Crawler:
 
     def load_page(self, url, withAjax=False, payload=None):
         # Add a delay between requests between (0..1)
-        time.sleep(random.uniform(0, 1))
+        time.sleep(random.uniform(5, 20))
         self.url = url
         # Get the page source of the requested url
         page_source = Utils.fetch_source(self.url, withAjax=withAjax, payload=payload)
@@ -42,11 +42,16 @@ class Utils:
 
     def fetch_source(url, stream=False, withAjax=False, payload=None):
         headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
-        if withAjax:
-            headers['X-Requested-With'] = 'XMLHttpRequest'
-            request = requests.post(url, data=payload, headers=headers)
-        else:
-            request = requests.get(url, headers=headers, stream=stream)
+        try:
+            if withAjax:
+                headers['X-Requested-With'] = 'XMLHttpRequest'
+                request = requests.post(url, data=payload, headers=headers)
+            else:
+                request = requests.get(url, headers=headers, stream=stream)
+        except http.client.RemoteDisconnected:
+            print('Remote disconnect, trying again...')
+            return fetch_source(url, stream, withAjax=withAjax, payload=payload)
+
         if stream:
             return b64encode(request.content)
         return request.text
